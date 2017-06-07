6dbd9e34950c99a096c432c52e17d5cae9f5bfdf
3787081814 <3787081814@pc301050m.ecc.u-tokyo.ac.jp>
2017-06-07 16:52:24 +0900

    bin,src replaced with week1

---
+++ week1/NumberReading.java
@@ -0,0 +1,23 @@
+import java.io.FileReader;
+import java.io.StreamTokenizer;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class NumberReading {
+    public static void main (String[] args) {
+        try {
+            FileReader fr = new FileReader("number.txt");
+            StreamTokenizer st = new StreamTokenizer(fr);
+            while (st.nextToken() != StreamTokenizer.TT_EOF) {
+                if(st.ttype == StreamTokenizer.TT_NUMBER) {
+                    System.out.println(st.nval); // nvalはdouble型
+                }
+            }
+            fr.close();
+        } catch (FileNotFoundException e) {
+            System.out.println ("this file was not found.");
+        } catch (IOException e) {
+            System.out.println ("Error: " + e);
+        }
+    }
+}
