269a317ea70f1cf0acc278f3118e837d9db4e25e
3787081814 <3787081814@pc301037m.ecc.u-tokyo.ac.jp>
2017-06-30 14:37:19 +0900

    数理演習2

--- TSP_1.py
+++ TSP_1.py
@@ -9,6 +9,39 @@ map = np.array([
 	[28, 6, 11, 5, inf]
 	])
 
-for i in range(5):
-	map[i,:] -= map[i,:].max
+S_col = 0
+S_row = 0
+G = 0
 
+for i in range(5):
+	S_row += map[i,:].max()
+	map[i,:] -= map[i,:].max()
+
+for i in range(5):
+	S_col += map[:,i].max()
+	map[:,i] -= map[:,i].max()
+	
+
+#R/*
+map <- t(matrix(c(Inf ,21, 7, 13, 15, 11 ,Inf , 19, 12, 25, 15, 24, Inf, 13, 5, 6, 17, 9, Inf, 22, 28, 6, 11, 5, Inf),ncol=5,nrow=5))
+map
+
+index=1:5
+min=Inf
+arg_min = c()
+for(i in index){
+for(j in index[-c(i)]){
+for(k in index[-c(i,j)]){
+for(l in index[-c(i,j,k)]){
+for(m in index[-c(i,j,k,l)]){
+sum =map[i,j]+map[j,k]+map[k,l]+map[l,m]+map[m,i]
+if(sum < min){ min = sum;  arg_min=c(i,j,k,l,m)}
+}
+}
+}
+}
+}
+min
+arg_min
+
+*/
